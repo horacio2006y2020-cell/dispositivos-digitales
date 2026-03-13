a5fec2cf357141b64b6c2735ac53ae938e03d966
horacio2006y2020-cell <horacio2006y2020@gmail.com>
2026-03-12 22:00:38 -0600

    final del ejercicio

--- dispositivos digitales/Program.cs
+++ dispositivos digitales/Program.cs
@@ -1,25 +1,5 @@
 ﻿using System; using System.Collections.Generic;
-class Dispositivo
-{
-    public string Nombre;
-    public double Rendimient1;
-    public double Rendimiento2;
-    public double Rendimiento3;
 
-    public double CalcularPromedio() 
-        {
-            return (Rendimient1 + Rendimiento2 + Rendimiento3) / 3;
-        }
-    public string ObtenerEstado()
-    {
-        if (CalcularPromedio() <= 80) return "Optimo";
-        else  return "Necesita mantenimiento";
-    }
-    public void MostrarDatos()
-    {
-        Console.WriteLine($"nombre: {Nombre} | Promedio: {CalcularPromedio():F2} | Estado: {ObtenerEstado()}");
-    }
-}
 List<Dispositivo> dispositivos = new List<Dispositivo>();
 Console.WriteLine("¿Cuántos dispositivos desea ingresar?");
 int n = int.Parse(Console.ReadLine());
@@ -34,7 +14,46 @@ for (int i = 0; i < n; i++)
     dispositivos.Add(dis);
 }
 double sumaPromedios = 0;
+Dispositivo mejor = dispositivos[0];
 
+Console.WriteLine("\n--- LISTADO DE DISPOSITIVOS ---");
+
+foreach (Dispositivo d in dispositivos)
+{
+    d.MostrarDatos();
+    sumaPromedios += d.CalcularPromedio();
+
+    if (d.CalcularPromedio() > mejor.CalcularPromedio())
+    {
+        mejor = d;
+    }
+    Console.WriteLine($"\nPromedio general del laboratorio: {sumaPromedios / dispositivos.Count:F2}");
+
+    Console.WriteLine("\nDispositivo con mejor rendimiento:");
+    mejor.MostrarDatos();
+}
+
+class Dispositivo
+{
+    public string Nombre;
+    public double Rendimient1;
+    public double Rendimiento2;
+    public double Rendimiento3;
+
+    public double CalcularPromedio()
+    {
+        return (Rendimient1 + Rendimiento2 + Rendimiento3) / 3;
+    }
+    public string ObtenerEstado()
+    {
+        if (CalcularPromedio() <= 80) return "Optimo";
+        else return "Necesita mantenimiento";
+    }
+    public void MostrarDatos()
+    {
+        Console.WriteLine($"nombre: {Nombre} | Promedio: {CalcularPromedio():F2} | Estado: {ObtenerEstado()}");
+    }
+}
 
 
 
